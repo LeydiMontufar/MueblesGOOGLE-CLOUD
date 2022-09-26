0386d0bc53b8498e1f546815ed9a8c3dd591e3b0
LeydiMontufar <leydimontufar16@gmail.com>
2022-09-25 22:45:42 -0500

    gg

--- handler.php
+++ handler.php
@@ -9,17 +9,50 @@ switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
     case '/pepe':
         require 'pepe.php';
         break;
-    case '/CrearUsuario':
-        require 'CrearUsuario.php';
-        break;
-    case '/LoginUsuario':
-        require 'LoginUsuario.php';
+    case '/Catalogo':
+        require 'Catalogo.php';
         break; 
     case '/CatalogoC':
         require 'CatalogoC.php';
-        break;  
+        break;
+    case '/CatalogoA':
+        require 'CatalogoA.php';
+        break; 
+    case '/index':
+        require 'index.php';
+        break;
     case '/index1':
         require 'index1.php';
+        break;
+    case '/indexA':
+        require 'indexA.php';
+        break;
+    case '/CrearCarrito':
+        require 'CrearCarrito.php';
+        break;
+    case '/CrearProducto':
+        require 'CrearProducto.php';
+        break; 
+    case '/CrearUsuario':
+        require 'CrearUsuario.php';
+        break;
+    case '/EditarProducto':
+        require 'EditarProducto.php';
+        break;  
+    case '/VerPedido':
+        require 'VerPedido.php';
+        break;
+    case '/LoginUsuario':
+        require 'LoginUsuario.php';
+        break;
+    case '/home':
+        require 'home.php';
+        break;
+    case '/homeA':
+        require 'homeA.php';
+        break;
+    case '/homeC':
+        require 'homeC.php';
         break;
     default:
         http_response_code(404);
